8fd317549c5e6e9a25806d5027990be694480821
AnthonyMDambrosio <anthonymdambrosio@gmail.com>
2015-11-10 14:55:00 -0800

    Create Stock.java

---
+++ src/InheritanceDesign/Stock.java
@@ -0,0 +1,37 @@
+/******************************************
+* Programmer : Anthony D'Ambrosio
+* Date       : 11/10/2015
+* Purpose    : Net Worth
+* Notes      :
+******************************************/
+package InheritanceDesign;
+
+
+public class Stock extends Security
+{
+    public Stock ( String name, double price, int quantity )
+    {
+        super( name, price, quantity);
+    }
+    
+    @Override
+    public double getAssetValue()
+    {
+        return price * quantity;
+    }
+    
+    @Override
+    public double getDebtValue() 
+    {
+        double dummy = 0; 
+        return dummy;
+    }
+    
+    @Override 
+    public String toString()
+    {
+        return this.getClass().getSimpleName()+ ", " + name + ", Shares Owned: " 
+                + quantity + ", Price: " + price 
+                + ", Value: " + getAssetValue(); 
+    }
+}
