c609270b1ee9300882d5998f28e8a98d63cf6fb5
AnthonyMDambrosio <anthonymdambrosio@gmail.com>
2015-11-10 14:56:24 -0800

    Create Debt.java

---
+++ src/InterfaceDesign/Debt.java
@@ -0,0 +1,12 @@
+/******************************************
+* Programmer : Anthony D'Ambrosio
+* Date       : 11/10/2015
+* Purpose    : Net Worth
+* Notes      :
+******************************************/
+package InterfaceDesign;
+
+public interface Debt
+{
+    public double getDebtAmount();
+}
