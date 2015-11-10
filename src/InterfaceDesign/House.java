a09c14da45d239893014fc45950cebdcc074c75a
AnthonyMDambrosio <anthonymdambrosio@gmail.com>
2015-11-10 14:56:36 -0800

    Create House.java

---
+++ src/InterfaceDesign/House.java
@@ -0,0 +1,39 @@
+/******************************************
+* Programmer : Anthony D'Ambrosio
+* Date       : 11/10/2015
+* Purpose    : Net Worth
+* Notes      :
+******************************************/
+package InterfaceDesign;
+
+public class House implements Debt, Asset
+{
+    private String name;
+    private double value, debt;
+    
+    public House(String name, double value, double debt)
+    {
+        this.name = name;
+        this.value = value;
+        this.debt = debt;
+    }
+    
+    @Override
+    public double getDebtAmount()
+    {
+        return debt;
+    }
+    
+    @Override
+    public double getAssetValue()
+    {
+        return value;
+    }
+    
+    @Override 
+    public String toString ()
+    {
+        return this.getClass().getSimpleName() + ", Value: " + value + 
+                ", Debt: " + debt + ", Net value: " + ( value - debt );
+    }
+}
