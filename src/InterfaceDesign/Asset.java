b74be497fd2bdd128ca06a399f4a6c858633d774
AnthonyMDambrosio <anthonymdambrosio@gmail.com>
2015-11-10 14:55:46 -0800

    Create Asset.java

---
+++ src/InterfaceDesign/Asset.java
@@ -0,0 +1,12 @@
+/******************************************
+* Programmer : Anthony D'Ambrosio
+* Date       : 11/10/2015
+* Purpose    : Net Worth
+* Notes      :
+******************************************/
+package InterfaceDesign;
+
+public interface Asset
+{
+    public double getAssetValue();
+}
