ba03ea2946cce1abb5a6b93455c7546e936c9d91
AnthonyMDambrosio <anthonymdambrosio@gmail.com>
2015-11-10 14:56:45 -0800

    Create Stock.java

---
+++ src/InterfaceDesign/Stock.java
@@ -0,0 +1,35 @@
+/******************************************
+* Programmer : Anthony D'Ambrosio
+* Date       : 11/10/2015
+* Purpose    : Net Worth
+* Notes      :
+******************************************/
+package InterfaceDesign;
+
+public class Stock implements Asset
+{
+    private String name;
+    private int shares;
+    private double value;
+    
+    public Stock(String name, int shares, double value)
+    {
+        this.name = name;
+        this.shares = shares;
+        this.value = value;
+    }
+    
+    @Override
+    public double getAssetValue()
+    {
+        return shares * value;
+    }
+    
+    @Override 
+    public String toString ()
+    {
+        return this.getClass().getSimpleName() + ", " + name + ", Shares: " 
+                + shares + ", Worth: " + value + ", Total: " + 
+                ( shares * value );
+    }
+}
